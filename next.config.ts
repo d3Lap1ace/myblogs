68d99ee9c8819c0618431ca578381eb39b42cd1b
v_lvhang02 <v_lvhang02@baidu.com>
2025-07-05 19:21:48 +0800

    chore: static export -> out

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output:'export',
+  basePath: '/myblogs',
+  assetPrefix: '/myblogs/',
+  images: { unoptimized: true }
 };
 
 export default nextConfig;
